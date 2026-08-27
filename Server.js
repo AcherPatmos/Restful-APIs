267f9157a280a4f64bb35438ffd2c9fa78933e2e
Patmos <a.mpakaniye@alustudent.com>
2026-08-27 20:04:57 +0400

    wrote the patch request route and fixed the url paths to all the other routes to match assignment requirements

--- Server.js
+++ Server.js
@@ -10,42 +10,75 @@ app.listen(port, () => {
 
 
 app.post("/v1/tasks", async (req, res) => {
-  const { title, completed } = req.body;
 
-  const [result] = await db.query(
-    "INSERT INTO tasks (title, completed) VALUES (?, ?)",
-    [title, completed ?? false],
-  );
+    const { title, completed } = req.body;
 
-  res
-    .status(201)
-    .json({ id: result.insertId, title, completed: completed ?? false });
+    const [result] = await db.query("INSERT INTO tasks (title, completed) VALUES (?, ?)", [title, completed ?? false]);
+
+    res.status(201).json({ id: result.insertId, title, completed: completed ?? false });
+
 });
 
-app.get("/tasks", async (req, res) => {
-  const [rows] = await db.query("SELECT * FROM tasks");
-  res.json(rows);
+app.get("/v1/tasks", async (req, res) => {
+
+        const [rows] = await db.query("SELECT * FROM tasks");
+
+        if (rows.length === 0) {
+            return res.status(404).json({"message": "No tasks found."});
+        }
+        res.json(rows);
+
 });
 
-app.get("/tasks/:id", async (req, res) => {
+app.get("/v1/tasks/:id", async (req, res) => {
 
     const [rows] = await db.query("SELECT * FROM tasks WHERE id = ?", [req.params.id]);
     if (rows.length===0) {
-        return res.status(404).json({"message": "No tasks found"});
+        return res.status(404).json({"message": "No tasks found with id " + req.params.id});
     }
     res.json(rows[0]);
 
 });
 
-app.patch("/tasks/:id", (req, res) => {
-  res.json({ message: `update task ${req.params.id}` });
+app.patch("/v1/tasks/:id", async (req, res) => {
+  const { title, completed } = req.body;
+
+  const fields = [];
+  const values = [];
+
+  if (title !== undefined) {
+    fields.push("title = ?");
+    values.push(title);
+  }
+
+  if (completed !== undefined) {
+    fields.push("completed = ?");
+    values.push(completed);
+  }
+
+  if (fields.length === 0) {
+    return res.status(400).json({ error: "No fields to update" });
+  }
+
+  values.push(req.params.id);
+
+  const [result] = await db.query(
+    `UPDATE tasks SET ${fields.join(", ")} WHERE id = ?`,
+    values
+  );
+
+  if (result.affectedRows === 0) {
+    return res.status(404).json({ error: "Task not found" });
+  }
+
+  res.json({ message: "Task with id" + req.params.id+ "updated" });
 });
 
-app.delete("/tasks/:id", async (req, res) => {
+app.delete("/v1/tasks/:id", async (req, res) => {
 
     const [result]= await db.query("DELETE FROM tasks where id = ?", [req.params.id]);
     if (result.affectedRows === 0) {
-        return res.status(404).json({"message": "No tasks found"});
+        return res.status(404).json({"message": "No tasks found with id " + req.params.id});
     }
     res.status(204).send();
 });
